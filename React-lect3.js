d6a9eef55721acaf21cfabef4bc2a8cf353f10a8
Ankit Singh <90319693+AnkitkumarSingh555@users.noreply.github.com>
2023-09-15 10:31:46 +0530

    laying foundation

--- React-lect3.js
+++ React-lect3.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom /clients";
-const Title=(<h1 className="head" tabIndex="5">
-Namaste React using jsx
+const Title=() =>(
+ <h1 className="head" tabIndex="5">
+Namaste React using JSX
 </h1>
 );
-
- const Headingcomponent=()=>(<div id="container">
+const Headingcomponent=()=>(
+  <div id="container">
+ {Title()}
  <Title/>
+ <Title> </Title>
+  
  <h1 className="heading">Namaste React functional component</h1>
  </div>
  );
