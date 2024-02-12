0ab5d4b4ac7f2b925af2b8e2f7454ac6013ac994
Ankit Singh <90319693+AnkitkumarSingh555@users.noreply.github.com>
2024-02-13 02:00:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,32 +1,17 @@
-const heading1=React.createElement ( 
-    "h1",
-{
-    id:"title",
-
-},
-      "Line 1"
- );
-      
-      
-const heading2=React.createElement ( 
-   "h2",
-{
-    id:"title",
-
-},
-      "Line 2"
+import  App from'./App.css';
+import Todo from './Todo;
+function App(){
+    return(
+<> 
+ <div className="app">
+     <Todolist/>
+</div>
 );
-    
-const container=React.createElement ( 
-    "div",
-{
-     id:"container",
-    
-
-}, 
-     [heading1,heading2 ] 
-);
-     
-const root=ReactDOM.createRoot(document.getElementById("root"));
-//passing a react element inside root
-root.render(container);
+}
+export default App;
+        
+        
+        
+        
+        
+</>  
