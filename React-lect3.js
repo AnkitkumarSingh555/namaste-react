f118ab294e40e7312ab04b4288b0825ea8b45c65
Ankit Singh <90319693+AnkitkumarSingh555@users.noreply.github.com>
2024-02-13 04:20:53 +0530

    Update React-lect3.js

--- React-lect3.js
+++ React-lect3.js
@@ -1,20 +1,53 @@
 import React from "react";
-import ReactDOM from "react-dom /clients";
-const Title=() =>(
- <h1 className="head" tabIndex="5">
-Namaste React using JSX
-</h1>
-);
-const Headingcomponent=()=>(
-  <div id="container">
- {Title()}
- <Title/>
- <Title> </Title>
+function Todolist(){
+   const [activity ,setActivity]=useState("");
+ const [listData,setlistData]=usestate([]);
+ function  add Activity(){
+       \\setlistData([....listData,activity])
+      \\console.log (listData)
+  setlistData((listData)=>{
+    const updatedList=[...listData,activity]
+    console.log(updatedList)
+    setActivity('');
+   return updatedList
+  })
   
- <h1 className="heading">Namaste React functional component</h1>
- </div>
- );
+}
+function removeActivity(i){
+  const updatedListData=listData.filter((elem,id)=>{
+    return i!=id;
+   )}
+ setListData(updatedlistData);
+}
+function removeAll(){
+  setListData([])
 
+}
+ return(
+  <>
+  <div className="container">
+      <div className="header">Todo List</div>
+      <input type="text" placeholder="Add Activity"  value={activity}  on change={(e)
+      =>setActivity(e.target.value)
+       <button onclick={addActivity}>App</button>
+       <p className='List_heading'>Here is your list:{")"} </p>
+       {listData!=[]&& listData.map((data,i)=>{
+        return(
+         <>
+         <p key={i}>
+         <div className="listData">{data}</div>
+         <div className"btn_position"><button onclick={()=>removeActivity
+           (i)}>remove(-)
+            ()</button></div>
+         </p>
+         </>
+        )
+        })}
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Headingcomponent/>);
+      {listData.length>=1&&  <button onClick={removeAll>RemoveAll</button>}
+     
+  <div>
+  </>
+ )
+}
+export default TodoList
